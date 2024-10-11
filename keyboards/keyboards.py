186f3e2f84a227f1d8987dfc72be429d116f2821
Nikita Pinegin <nobodyni@icloud.com>
2024-10-12 01:49:37 +0300

    Изменил названия переменных

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,13 +5,13 @@ from lexicon.lexicon_ru import LEXICON_RU
 
 button_city = KeyboardButton(text=LEXICON_RU['city_button'])  # кнопка для ввода города пользователя
 
-yes_no_kb_builder = ReplyKeyboardBuilder()  # инициализируем билдер для кнопки button_city
+kb_builder = ReplyKeyboardBuilder()  # инициализируем билдер для кнопки button_city
 
-yes_no_kb_builder.row(button_city, width=2)  # добавляем кнопки в билдер с аргументом width=2
+kb_builder.row(button_city, width=2)  # добавляем кнопки в билдер с аргументом width=2
 
 # создаем клавиатуру с кнопкой 'Введите город'
 # будем сворачивать кнопку после нажатия на нее
-yes_no_kb: ReplyKeyboardMarkup = yes_no_kb_builder.as_markup(
+kb: ReplyKeyboardMarkup = kb_builder.as_markup(
     one_time_keyboard=True,
     resize_keyboard=True
 )
