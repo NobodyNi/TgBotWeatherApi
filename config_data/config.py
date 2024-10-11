49e1eafa5aca9dd355210f332128e880bac7c814
Nikita Pinegin <nobodyni@icloud.com>
2024-10-12 00:52:58 +0300

    Добавил конфиг

---
+++ config_data/config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:    # автоматическое определение специальных методов для TgBot
+    token: str  # для доступа к телеграм боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:  # будем возвращать объект типа Config
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
