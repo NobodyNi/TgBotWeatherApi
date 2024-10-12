89dd4d135610cf7f98418d116acfd19fd8850152
Nikita Pinegin <nobodyni@icloud.com>
2024-10-12 15:53:25 +0300

    Добавил проверку в services.py

--- services/services.py
+++ services/services.py
@@ -1,11 +1,14 @@
 import requests
 
 from config_data.config import Config, load_config
+from lexicon.lexicon_ru import LEXICON_RU
 
 
 def get_weather(city):
-    config: Config = load_config()
-    base_url = 'http://api.openweathermap.org/data/2.5/weather'
+    config: Config = load_config()  # загружаем конфиг
+    base_url = 'http://api.openweathermap.org/data/2.5/weather'  # url для запроса к API
+
+    # параметры для запроса
     params = {
         'q': city,
         'appid': config.tg_bot.weather_api,
@@ -13,13 +16,17 @@ def get_weather(city):
         'lang': 'ru'
     }
 
-    response = requests.get(base_url, params=params)
-    response.raise_for_status()
-    data = response.json()
+    response = requests.get(base_url, params=params)  # отправляет get запрос к API с параметрами
 
-    temperature = data['main']['temp']
-    weather = data['weather'][0]['description']
-    humidity = data['main']['humidity']
+    # если город не найден, API возвращает статус 404
+    if response.status_code == 404:
+        return f"{LEXICON_RU['no_info']} '{city}'"
+
+    response.raise_for_status()  # если есть ошибки кроме 404, вызывает исключение
+    data = response.json()       # словарь с данными о погоде
+
+    temperature = data['main']['temp']           # температура
+    weather = data['weather'][0]['description']  # погода
+    humidity = data['main']['humidity']          # влажность
 
     return f"{city} - {weather}\nТемпература - {temperature}°C\nВлажность - {humidity}%"
-
